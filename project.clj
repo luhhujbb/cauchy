1173f8d66a3a9bbc05ac632d492d0afc3fd58412
Jean-Baptiste Besselat <jean-baptiste.besselat@linkfluence.net>
2016-03-15 15:15:51 +0100

    [UPDATE]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cauchy "0.1.1"
+(defproject cauchy "0.1.2"
   :description "Cauchy is an agent for Riemann"
   :url "https://github.com/pguillebert/cauchy"
   :scm {:name "git"
@@ -12,14 +12,14 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :signing {:gpg-key "93FEB8D7"}
   :deploy-repositories [["clojars" {:creds :gpg}]]
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.slf4j/log4j-over-slf4j "1.7.12"]
-                 [riemann-clojure-client "0.3.2"]
-                 [jarohen/chime "0.1.6"]
-                 [puppetlabs/trapperkeeper "1.1.1"]
+                 [org.slf4j/log4j-over-slf4j "1.7.19"]
+                 [riemann-clojure-client "0.4.2"]
+                 [jarohen/chime "0.1.9"]
+                 [puppetlabs/trapperkeeper "1.3.0"]
                  [indigenous "0.1.0"]
-                 [bultitude "0.2.6"]
+                 [bultitude "0.2.8"]
                  [sigmund "0.1.1" :exclusions [log4j sigar/sigar-native-deps]]
                  [stask/sigar-native-deps "1.6.4"]]
   :aot [puppetlabs.trapperkeeper.main]
