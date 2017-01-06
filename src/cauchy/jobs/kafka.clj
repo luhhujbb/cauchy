2f3ffcc8dc537bc6b981246782f810025c9b150f
Pierre Padrixe <pierre.padrixe@linkfluence.com>
2017-01-06 17:16:35 +0100

    Reorder kafka fn

--- src/cauchy/jobs/kafka.clj
+++ src/cauchy/jobs/kafka.clj
@@ -6,14 +6,14 @@
   [group]
   (:out (sh "bash" "-c" "/rtgi/ext/kafka/bin/kafka-consumer-groups.sh --new-consumer --bootstrap-server kafka-10-a.prod.aws.rtgi.eu:9092 --group group --describe|tr -s ' '|cut -d ' ' -f 6,7,3|tail -n +2|sed 's/ /,/g'")))
 
+(defn keywordize-consumer-stats
+  [parsed-stats]
+  (map #(zipmap [:id :lag :owner] %) parsed-stats))
+
 (defn parse-consumer-stats
   [stats]
   (keywordize-consumer-stats
    (map #(str/split % #",") (str/split-lines stats))))
-
-(defn keywordize-consumer-stats
-  [parsed-stats]
-  (map #(zipmap [:id :lag :owner] %) parsed-stats))
 
 (defn get-consumer-lags
   [group]
