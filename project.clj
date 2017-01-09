675f7534d8dc354347f4f8f6aaf36ad66153d631
Jean-Baptiste Besselat <jean-baptiste.besselat@linkfluence.net>
2017-01-09 15:58:58 +0100

    [FIX] support client connection loss, and reconnect

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cauchy "0.1.35"
+(defproject cauchy "0.1.36"
   :description "Cauchy is an agent for Riemann"
   :url "https://github.com/pguillebert/cauchy"
   :scm {:name "git"
--- src/cauchy/output/riemann.clj
+++ src/cauchy/output/riemann.clj
@@ -1,5 +1,6 @@
 (ns cauchy.output.riemann
-  (:require [riemann.client :as rc]))
+  (:require [riemann.client :as rc])
+  (:import (java.io IOException)))
 
 (def rc (atom nil))
 
@@ -10,4 +11,13 @@
 
 (defn send!
   [msg]
+  (when-not (rc/connected? @rc)
+    (try (rc/reconnect! @rc)
+         (catch IOException e
+           (rc/flush! @rc))))
   (rc/send-event @rc msg))
+
+(defn close!
+  [msg]
+  (rc/close! @rc)
+  (reset! rc nil))
