3b3dae28270364e167d3c310f72393c32523e176
Matthieu Marie <matthieu.marie@linkfluence.com>
2017-06-07 17:38:37 +0200

    dirty shit fix to make redis stats works

--- src/cauchy/jobs/redis.clj
+++ src/cauchy/jobs/redis.clj
@@ -20,16 +20,16 @@
 		  evicted_keys used_memory_lua instantaneous_ops_per_sec rejected_connections keyspace_misses 
           keyspace_hits connected_clients rejected_connections total_commands_processed]} (mergetomap)]
      (cond (= instantaneous_ops_per_sec 0) (def state "warn"):else (def state "ok"))
-     [{:service "total_net_input_bytes" :state defaultstate :metric total_net_input_bytes}
-     {:service "total_net_output_bytes" :state defaultstate :metric total_net_output_bytes}
-      {:service "expired_keys" :state defaultstate :metric expired_keys }
-      {:service "evicted_keys" :state defaultstate :metric evicted_keys}
-      {:service "used_memory_lua" :state defaultstate :metric used_memory_lua }
-      {:service "instantaneous_ops_per_sec" :state state :metric instantaneous_ops_per_sec}
-      {:service "rejected_connections" :state defaultstate :metric rejected_connections}
-      {:service "keyspace_misses" :state defaultstate :metric keyspace_misses}
-      {:service "keyspace_hits" :state defaultstate :metric keyspace_hits }
-      {:service "total_commands_processed" :state defaultstate :metric total_commands_processed }
-      {:service "connected_clients" :state defaultstate :metric connected_clients }
+     [{:service "total_net_input_bytes" :state defaultstate :metric (Long/parseLong total_net_input_bytes)}
+     {:service "total_net_output_bytes" :state defaultstate :metric (Long/parseLong total_net_output_bytes)}
+      {:service "expired_keys" :state defaultstate :metric (Long/parseLong expired_keys) }
+      {:service "evicted_keys" :state defaultstate :metric (Long/parseLong evicted_keys)}
+      {:service "used_memory_lua" :state defaultstate :metric (Long/parseLong used_memory_lua) }
+      {:service "instantaneous_ops_per_sec" :state state :metric (Long/parseLong instantaneous_ops_per_sec)}
+      {:service "rejected_connections" :state defaultstate :metric (Long/parseLong rejected_connections)}
+      {:service "keyspace_misses" :state defaultstate :metric (Long/parseLong keyspace_misses)}
+      {:service "keyspace_hits" :state defaultstate :metric (Long/parseLong keyspace_hits) }
+      {:service "total_commands_processed" :state defaultstate :metric (Long/parseLong total_commands_processed)}
+      {:service "connected_clients" :state defaultstate :metric (Long/parseLong connected_clients) }
       {:service "hitrate" :state defaultstate :metric (float(hitrate (read-string keyspace_hits) (read-string keyspace_misses)))}]
 ))
