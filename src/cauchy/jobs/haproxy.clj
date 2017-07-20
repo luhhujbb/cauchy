cb844c4a929d1840feaae4f7a27dd2467d0e83ad
Matthieu Marie <matthieu.marie@linkfluence.com>
2017-07-20 15:50:56 +0200

    adding haproxy job

---
+++ src/cauchy/jobs/haproxy.clj
@@ -0,0 +1,47 @@
+(ns cauchy.jobs.haproxy
+  (:require [clj-http.client :as http]))
+
+(def state (atom {:state "warning"}))
+
+(defn parse-int [s]
+   (Integer. (re-find  #"\d+" s )))
+
+(defn haproxy-fetch-metrics
+  "Get uberfeedr metrics"
+  [{:keys [host port] :or {host "localhost" port 443}}]
+    (let [url (str "https://pw" host ":" port "/haproxy-stats;csv")]
+        (:body (http/get url ))))
+
+(defn extract-fields []
+  (first (map (fn [field] (clojure.string/split field #"," )) 
+      (clojure.string/split (haproxy-fetch-metrics []) #"\n"))))
+
+(defn kw-extract-fields []
+  (vec (map (fn [x] (keyword (clojure.string/replace x  #"# " "" ))) (extract-fields))))
+
+(defn get-raw-stats []
+  (drop 1 (map (fn [x] (clojure.string/split x #"," )) 
+      (clojure.string/split (haproxy-fetch-metrics []) #"\n"))))
+
+(defn json-stats []
+  (vec(map (fn [stats] (zipmap (kw-extract-fields) stats)) (get-raw-stats))))
+
+(defn pretty-stats []
+  (map 
+     (fn [x]
+        (let [{:keys [:weight status svname smax hrsp_2xx hrsp_3xx hrsp_4xx pxname] } x ]
+          {:pxname pxname :status status :svname svname :smax smax :weight weight 
+           :hrsp_2xx hrsp_2xx :hrsp_3xx hrsp_3xx :hrsp_4xx hrsp_4xx}))
+  (json-stats) ))
+
+(defn haproxy-stats []
+ (vec 
+    (map
+       (fn [x]
+      (let [{:keys [pxname status svname weight hrsp_3xx hrsp_2xx hrsp_4xx]} x]
+         (cond
+           (true? (= svname "BACKEND")) (reset! state "critical") :else (reset! state "warning"))
+          {:service (str pxname"."(clojure.string/replace svname #"\." "_")".status") 
+            :state (cond (true? (= status "DOWN")) @state :else "ok") 
+            :metric (cond (true? (or(= status "UP")(= status "OPEN")) ) 1 :else 0)}
+          ))(pretty-stats))))
