31e69f299770eac65b67388d5634ea8335c72ea9
Matthieu Marie <matthieu.marie@linkfluence.com>
2017-08-24 11:31:15 +0200

    adding try/catch for jmx job

--- src/cauchy/jobs/jmx.clj
+++ src/cauchy/jobs/jmx.clj
@@ -1,8 +1,10 @@
 (ns cauchy.jobs.jmx
     (require [clojure.java.jmx :as jmx]
+             [clojure.tools.logging :as log]
              [clojure.string :as str]))
 
 (defn memory-stats [{:keys [host port] :or {host "localhost" port "9998"}}]
+   (try
     (jmx/with-connection {:host host, :port port}
      (into []
       (mapcat 
@@ -10,9 +12,11 @@
         (map (fn [[metric value]] 
          {:service (str (name k) "." (name metric)) :metric value}) 
         v)) 
-      (jmx/read "java.lang:type=Memory" [:HeapMemoryUsage :NonHeapMemoryUsage])))))
+      (jmx/read "java.lang:type=Memory" [:HeapMemoryUsage :NonHeapMemoryUsage]))))
+   (catch Exception e (log/error "cant get mem stats for jmx on " host " port " port))))
 
 (defn gc-stats [{:keys [host port gctype] :or {host "localhost" port "9998" gctype "PS MarkSweep"}}]
+  (try
    (jmx/with-connection {:host host, :port port}
     (let [{:keys [GcThreadCount duration endTime memoryUsageBeforeGc memoryUsageAfterGc] :as json} (jmx/read (format "java.lang:type=GarbageCollector,name=%s" gctype) :LastGcInfo ) 
            gc-details (select-keys json [:memoryUsageBeforeGc :memoryUsageAfterGc])]
@@ -29,19 +33,22 @@
                 {:service (format "%s.%s.%s.max" (str/replace gctype #" " "_") (name memory-type) (str/replace (name rubrique-gc)#" " "_")) :metric (:max (:value vals-rubrique-gc))}
                 {:service (format "%s.%s.%s.used" (str/replace gctype #" " "_") (name memory-type) (str/replace (name rubrique-gc)#" " "_")) :metric (:used (:value vals-rubrique-gc))}])
                (select-keys vals-memory-type [(keyword "PS Eden Space") (keyword "PS Survivor Space") (keyword "PS Old Gen")] )))
-           gc-details))))))
+           gc-details)))))
+             (catch Exception e (log/error "cant get gc stats for jmx on " host " port " port))))
 
 (defn threads-stats [{:keys [host port] :or {host "localhost" port "9998"}}]
+    (try
      (jmx/with-connection {:host host, :port port}
        (let [{:keys [ThreadCount PeakThreadCount DaemonThreadCount]} (select-keys (jmx/mbean "java.lang:type=Threading") [:ThreadCount :PeakThreadCount :DaemonThreadCount])]
         [{:service (format "Threading.%s" (name 'ThreadCount)) :metric ThreadCount}
          {:service (format "Threading.%s" (name 'PeakThreadCount)) :metric PeakThreadCount}
-         {:service (format "Threading.%s" (name 'DaemonThreadCount)) :metric DaemonThreadCount}])))
+         {:service (format "Threading.%s" (name 'DaemonThreadCount)) :metric DaemonThreadCount}]))
+       (catch Exception e (log/error "cant get threads stats for jmx on " host " port " port))))
 
 
 (defn stats [{:keys [host port] :or {host "localhost" port "9998"}}]
-  (into []
-   (concat
+   (into []
+    (concat
      (memory-stats {:host host :port port})
      (gc-stats {:host host :port port :gctype "PS MarkSweep"})
      (threads-stats {:host host :port port})
