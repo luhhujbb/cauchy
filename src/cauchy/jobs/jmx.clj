377df8ecd31d6ac7c733573e6b0107ba4081a151
Matthieu Marie <matthieu.marie@linkfluence.com>
2017-09-26 15:02:46 +0200

    adding possibility to retrieve g1gc metrics

--- src/cauchy/jobs/jmx.clj
+++ src/cauchy/jobs/jmx.clj
@@ -15,10 +15,11 @@
       (jmx/read "java.lang:type=Memory" [:HeapMemoryUsage :NonHeapMemoryUsage]))))
    (catch Exception e (log/error "cant get mem stats for jmx on " host " port " port))))
 
-(defn gc-stats [{:keys [host port gctype] :or {host "localhost" port "9998" gctype "PS MarkSweep"}}]
+(defn gc-stats [{:keys [host port gctype prefix-gc] :or {host "localhost" port "9998" gctype "PS MarkSweep"}}]
   (try
    (jmx/with-connection {:host host, :port port}
-    (let [{:keys [GcThreadCount duration endTime memoryUsageBeforeGc memoryUsageAfterGc] :as json} (jmx/read (format "java.lang:type=GarbageCollector,name=%s" gctype) :LastGcInfo ) 
+    (let [{:keys [GcThreadCount duration endTime memoryUsageBeforeGc memoryUsageAfterGc] :as json} 
+           (jmx/read (format "java.lang:type=GarbageCollector,name=%s" gctype) :LastGcInfo ) 
            gc-details (select-keys json [:memoryUsageBeforeGc :memoryUsageAfterGc])]
         (into []
          (apply concat
@@ -29,12 +30,17 @@
             (fn [[memory-type vals-memory-type]]
               (map
                 (fn [[rubrique-gc vals-rubrique-gc]] 
-                [{:service (format "%s.%s.%s.committed" (str/replace gctype #" " "_") (name memory-type) (str/replace (name rubrique-gc)#" " "_")  ) :metric (:committed (:value vals-rubrique-gc))}
-                {:service (format "%s.%s.%s.max" (str/replace gctype #" " "_") (name memory-type) (str/replace (name rubrique-gc)#" " "_")) :metric (:max (:value vals-rubrique-gc))}
-                {:service (format "%s.%s.%s.used" (str/replace gctype #" " "_") (name memory-type) (str/replace (name rubrique-gc)#" " "_")) :metric (:used (:value vals-rubrique-gc))}])
-               (select-keys vals-memory-type [(keyword "PS Eden Space") (keyword "PS Survivor Space") (keyword "PS Old Gen")] )))
+                [{:service (format "%s.%s.%s.committed" (str/replace gctype #" " "_") (name memory-type) (str/replace (name rubrique-gc)#" " "_")  ) 
+                  :metric (:committed (:value vals-rubrique-gc))}
+                {:service (format "%s.%s.%s.max" (str/replace gctype #" " "_") (name memory-type) (str/replace (name rubrique-gc)#" " "_")) 
+                 :metric (:max (:value vals-rubrique-gc))}
+                {:service (format "%s.%s.%s.used" (str/replace gctype #" " "_") (name memory-type) (str/replace (name rubrique-gc)#" " "_")) 
+                 :metric (:used (:value vals-rubrique-gc))}])
+              (select-keys vals-memory-type [(keyword (format "%s Eden Space" prefix-gc)) 
+                                             (keyword (format "%s Survivor Space" prefix-gc)) 
+                                             (keyword (format "%s Old Gen" prefix-gc ))])))
            gc-details)))))
-             (catch Exception e (log/error "cant get gc stats for jmx on " host " port " port))))
+             (catch Exception e (log/error "cant get gc stats for jmx on " host " port " port ":" e))))
 
 (defn threads-stats [{:keys [host port] :or {host "localhost" port "9998"}}]
     (try
@@ -46,10 +52,16 @@
        (catch Exception e (log/error "cant get threads stats for jmx on " host " port " port))))
 
 
-(defn stats [{:keys [host port] :or {host "localhost" port "9998"}}]
+(defn stats [{:keys [host port gc-type] :or {host "localhost" port "9998" gc-type "PS"}}]
    (into []
     (concat
-     (memory-stats {:host host :port port})
-     (gc-stats {:host host :port port :gctype "PS MarkSweep"})
-     (threads-stats {:host host :port port})
-     (gc-stats {:host host :port port :gctype "PS Scavenge"}))))
+    (memory-stats {:host host :port port})
+     (cond
+      (= gc-type "G1GC")
+       (gc-stats {:host host :port port :gctype "G1 Young Generation" :prefix-gc "G1"})
+     :else 
+       (do
+       (gc-stats {:host host :port port :gctype "PS MarkSweep" :prefix-gc "PS" })
+       (gc-stats {:host host :port port :gctype "PS Scavenge" :prefix-gc "PS" })))
+    (threads-stats {:host host :port port})
+     )))
