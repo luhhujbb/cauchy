8c4a6d367bc231fdf9871c6a1b3863be75fec1e7
Jean-Baptiste Besselat <jean-baptiste.besselat@linkfluence.net>
2017-12-18 16:57:26 +0100

    [LOG] try to reconnect every 200 msgs

--- src/cauchy/output/riemann.clj
+++ src/cauchy/output/riemann.clj
@@ -4,8 +4,8 @@
   (:import (java.io IOException)))
 
 (def rc (atom nil))
-
 (def r-conf (atom nil))
+(def msg-count (atom 0))
 
 (defn init!
   [conf]
@@ -14,10 +14,13 @@
 
 (defn send!
   [msg]
-  (when-not (r/connected? @rc)
-    (try (r/reconnect! @rc)
+  (swap! msg-count inc)
+  (when (= 0 (mod msg-count 50))
+    ;;since this happen only on network issue, we accept loss of 50 msgs
+    (when-not (r/connected? @rc)
+      (try (r/reconnect! @rc)
          (catch Exception e
-           (log/error "[RIEMANN] fail to reconnect" e))))
+           (log/error "[RIEMANN] fail to reconnect" e)))))
     (when (r/connected? @rc)
       (r/send-event @rc msg)))
 
